8785fbbc3aaddf3cd0c900d3569ec3e8ddc81822
Tristan Erney <tristanerney@yahoo.com>
2025-04-24 22:09:17 -0400

    Update build files

--- build.zig
+++ build.zig
@@ -4,13 +4,7 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const dep_opts = .{
-        .target = target,
-        .optimize = optimize,
-    };
-    _ = dep_opts;
-
-    _ = b.addModule("zig-toolkit", .{
+    _ = b.addModule("zig_toolkit", .{
         .root_source_file = b.path("toolkit.zig"),
         .target = target,
         .optimize = optimize,
